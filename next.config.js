16fd89f2a8dee19deb921f30d134ef7595e2e3b8
Liaksej <tarusov.liaksej@gmail.com>
2023-09-20 23:08:57 +0200

    Update next.config.js with output configuration
    In this commit, the next.config.js file was modified to include the output configuration. This was necessary to guide the project on how to handle the output. The export method was set to be used and a commented section for basepath was included for potential future use. This aims to improve project flexibility.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  output: "export",
+  // basePath: "/ra-components-store-layouts",
+};
+
+module.exports = nextConfig;
